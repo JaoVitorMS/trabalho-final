1d4ff99d98d3592212b8dc6e183b6a214008689b
João Vitor <joao.vitorms1509@gmail.com>
2024-06-21 20:01:30 -0300

    Criação das classes restantes

---
+++ src/br/com/pucrs/trabalhofinal/clients/Membro.java
@@ -0,0 +1,5 @@
+package br.com.pucrs.trabalhofinal.clients;
+
+public class Membro {
+    weghwiruogb
+}
---
+++ src/br/com/pucrs/trabalhofinal/produtcs/CadastroBicicletas.java
@@ -0,0 +1,4 @@
+package br.com.pucrs.trabalhofinal.produtcs;
+
+public class CadastroBicicletas {
+}
