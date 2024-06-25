e8d2e76d5632fc3b522dc0cee42fcee743b2e17a
João Vitor <joao.vitorms1509@gmail.com>
2024-06-25 15:53:23 -0300

    Adição de métodos

--- src/br/com/pucrs/trabalhofinal/clients/CadastroMembro.java
+++ src/br/com/pucrs/trabalhofinal/clients/CadastroMembro.java
@@ -1,4 +1,29 @@
 package br.com.pucrs.trabalhofinal.clients;
 
-public class CadastroMembro {
+public class CadastroMembro{
+
+    private Membro[] membros = new Membro[10];
+
+
+    public void adicionarMembro (Membro membro) {
+        boolean adicionou = false;
+        for (int i = 0; i < membros.length; i++) {
+            if (membros[i] == null) {
+                membros[i] = membro;
+                adicionou = true;
+                break;
+            }
+            if (!adicionou) {
+                System.out.println("Capacidade maxima atingida");
+            }
+        }
+    }
+
+    public void exibirMembros () {
+        for (int i = 0; i < membros.length; i++) {
+            if (membros[i] != null) {
+                System.out.println(membros[i].toString());
+            }
+        }
+    }
 }
