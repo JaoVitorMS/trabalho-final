848e3489bbd94fe662547794a35a7c8d1e7ef1a0
João Vitor <joao.vitorms1509@gmail.com>
2024-06-25 21:53:17 -0300

    teste dos novos métodos

--- src/Clube.java
+++ src/Clube.java
@@ -8,11 +8,16 @@ public class Clube {
         Membro cliente1 = new Membro(123, "Paula Tejano", "Pirituba City");
         Membro cliente2 = new Membro(1234, "Deyde Costa", "Pirituba City");
         Membro cliente3 = new Membro(1235, "Tekomo Nakama", "Pirituba City");
+        Membro cliente4 = new Membro(123456, "Mia Aronba", "Pirituba City");
 
         cadastro.adicionarMembro(cliente1);
         cadastro.adicionarMembro(cliente2);
         cadastro.adicionarMembro(cliente3);
-        cadastro.exibirMembros();
+        cadastro.adicionarMembro(cliente4);
+        cadastro.mostraMembros();
+        System.out.println("---------------------------");
+        cadastro.buscarPeloNome("Mia Aronba");
+        System.out.println("---------------------------");
 
     }
 }
